e179af00d28839a1820526098e2746d57d3bef3f
iamfilipovich <iamfilipovich@gmail.com>
2023-09-19 03:28:49 +0200

    update final

--- WebApplication1/Controllers/ParametriController.cs
+++ WebApplication1/Controllers/ParametriController.cs
@@ -1,44 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
-using System.Security;
 using WebApplication1.Data;
 using WebApplication1.Models;
 
-namespace WebApplication1.Controllers;
-
+namespace WebApplication1.Controllers
+{
     public class ParametriController : Controller
-    { 
+    {
         private readonly ApplicationDbContext _db;
 
-        public ParametriController (ApplicationDbContext db)
+        public ParametriController(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        
         public IActionResult Index()
         {
+            if (string.IsNullOrEmpty(ViewData["PadNapona"] as string))
+            {
+                ViewData["PadNapona"] = ""; // Default value
+            }
+
             IEnumerable<Parametri> objParametriList = _db.Parametri.ToList();
             return View(objParametriList);
         }
 
-        //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Racunaj(String povrsina, String snaga, String duzina, String napon)
+        public IActionResult HandleButtonClick()
         {
-            var u = float.Parse(napon);
-            var l = float.Parse(duzina);
-            var p = float.Parse(snaga);
-            var s = float.Parse(povrsina);
 
-            var otpor = _db.Parametri.Where(e => e.povrsina_presjeka == p).Select(e => e.otpor_vodica).FirstOrDefault();
-            var padNapona = ((p * l * otpor) / (u * u)) * 100;
-            ViewBag.padNapona = padNapona;
+            var povrsina = Convert.ToDouble(Request.Form["povrsina"]);
+            var otpor = _db.Parametri.Where(e => e.povrsina_presjeka == povrsina).Select(e => e.otpor_vodica).FirstOrDefault();
+            var snaga = Convert.ToInt32(Request.Form["snaga"]);
+            var duzina = Convert.ToInt32(Request.Form["duzina"]);
+            var napon = Convert.ToInt32(Request.Form["napon"]);
 
-            return View();   
+            if (povrsina == 0)
+            {
+                ViewData["WarningMessage"] = "Površina ima vrijednost 0. Upišite novu vrijednost.";
+            } else if (snaga == 0 )
+            {
+                ViewData["WarningMessage"] = "Snaga ima vrijednost 0. Upišite novu vrijednost.";
+            } else if (duzina == 0 )
+            {
+                ViewData["WarningMessage"] = "Dužina ima vrijednost 0. Upišite novu vrijednost.";
+            } else if (napon == 0)
+            {
+                ViewData["WarningMessage"] = "Napon ima vrijednost vrijednost 0. Upišite novu vrijednost.";
+            } else if (napon == 400)
+            {
+                ViewData["povrsina"] = povrsina;
+                ViewData["snaga"] = snaga;
+                ViewData["duzina"] = duzina;
+                ViewData["napon"] = napon;
+
+                double padNapona = ((snaga * duzina * otpor) / (napon * napon)) * 100;
+                ViewData["PadNapona"] = padNapona;
+            }
+            else
+            {
+                ViewData["povrsina"] = povrsina;
+                ViewData["snaga"] = snaga;
+                ViewData["duzina"] = duzina;
+                ViewData["napon"] = napon;
+
+                double padNapona = ((2 * snaga * duzina * otpor) / (napon * napon)) * 100;
+                ViewData["PadNapona"] = padNapona;
+            }
+            return View("Index");
         }
     }
-
-
+}
---
+++ WebApplication1/Models/ParametriFormula.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class ParametriFormula
+    {
+        public float povrsina { get; set; }
+        public float otpor { get; set; }
+        public int snaga { get; set; }
+        public int napon { get; set; }
+        public int duzina { get; set; }
+        public float rezultat { get; set; }
+
+    }
+}
